9951d2087e4305910dcfa34bf3ca00ca7f2ead24
Gaurav Sharma <gauravsharma01042000@gmail.com>
2025-05-03 22:44:40 +0530

    fibonacci logic update

--- Daily-Challenges/Week-1/day3.py
+++ Daily-Challenges/Week-1/day3.py
@@ -50,10 +50,8 @@ class Solution:
         return True
     
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
+        if n <= 1:
+            return n
         return self.fib(n - 1) + self.fib(n - 2)
         
 if __name__ == '__main__':
